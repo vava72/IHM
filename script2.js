62de2dd6ebc0722dae92ead7b6a999f13f0d5669
vava72 <i2200197@univ-lemans.fr>
2022-09-28 18:11:52 +0200

    test

--- script2.js
+++ script2.js
@@ -1,67 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.onload = function() {
+    document.getElementById("adresse").addEventListener("input", autocompleteAdresse, false);
+};
 
-    let inputAdresse = document.querySelector('.adresse>input');
-    let inputcp = document.querySelector('.cp>input');
-    let inputville = document.querySelector('.ville>input');
-    let list_adresse = document.querySelector(".list_adresse");
-    let allAdresses = document.querySelectorAll('ul>li');
-
-    inputAdresse.addEventListener('input', afficherVilles);
-
-    function getInfos(adresse) {
-        let url = "https://api-adresse.data.gouv.fr/search/?q=";
-        let words = adresse.split(' ');
-        let param = "";
-
-        words.forEach(element => {
-            if (param === "") {
-                param = element;
-            } else {
-                param = param + '+' + element;
-            }
-        });
-
-        let promise = new Promise((resolve, reject) => {
-            window.fetch(url + param)
-                .then(response => response.json())
-                .then(infos => {
-                    if (infos.features.length !== 0) {
-                        let informations = [];
-                        infos.features.forEach(element => {
-                            let {properties: {city},properties: {postcode},properties: {name},properties: {label}} = element;
-                            informations.push({city,postcode,name,label});
-                            resolve(informations);
-                        });
-                    }
-
-                })
-                .catch(error => reject(error));
-        });
-        return promise;
-    }
-
-
-    function afficherVilles(e) {
-        console.log("test");
-        inputcp.value = "";
-        inputville.value = "";
-        // list_adresse.innerHTML = "";
-        getInfos(e.target.value).then(response => {
-            response.forEach(element => {
-                list_adresse.insertAdjacentHTML("afterbegin","<li class='adresse' data-cp='"+element.postcode+"' data-city='"+element.city+"' data-name='"+element.name+"'>"+element.label+"</li>");
+function autocompleteAdresse() {
+        
+    var inputValue = document.getElementById("adresse").value;
+    if (inputValue) {
+        fetch(setQuery(inputValue))
+            .then(function (response) {
+                response.json().then(function (data) {
+                    displaySelection(data);
+                });
             });
-            let adresse = document.querySelectorAll('.adresse');
-            if(adresse.length !== 0){
-                adresse.forEach(elt => {
-                    elt.addEventListener('click', (e)=>{
-                        list_adresse.innerHTML = "";
-                        let target = e.target;
-                        inputcp.value = target.dataset.cp;
-                        inputville.value = target.dataset.city;
-                        inputAdresse.value = target.dataset.name;
-                    })
-                })
-            }
-        }).catch(error => console.log(error));
+    } else {
+        select.style.display = "none";
     }
-});
+};
+
+function displaySelection(response) {
+    if (Object.keys(response.features).length > 0) {
+        select.style.display = "block";
+        select.removeChild(select.firstChild);
+        var ul = document.createElement('ul');
+        select.appendChild(ul);
+        response.features.forEach(function (element) {
+            var li = document.createElement('li');
+            var ligneAdresse = document.createElement('span');
+            var infosAdresse = document.createTextNode(element.properties.postcode + ' ' + element.properties.city);
+            ligneAdresse.innerHTML = element.properties.name;
+            li.onclick = function () { selectAdresse(element); };
+            li.appendChild(ligneAdresse);
+            li.appendChild(infosAdresse);
+            ul.appendChild(li);
+        });
+    } else {
+        select.style.display = "none";
+    }
+}
